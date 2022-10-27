4efdae64d3d78289a8fc8b735e2f305b2cf7c34f
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-10-27 17:23:05 -0500

    [ADDED] Socials

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,12 @@ import React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import {
+  faGithub,
+  faWhatsapp,
+  faInstagram,
+  faDiscord,
+} from "@fortawesome/free-brands-svg-icons";
 
 /**
  * https://fontawesome.com/icons
@@ -14,9 +19,18 @@ const Footer = () => {
   return (
     <footer>
       <div>Raul Rodrigues &#169; 2022</div>
-      <a className="social-icons" href="https://github.com/luarrekcah">
-        <FontAwesomeIcon icon={faGithub} />
-      </a>
+        <a className="social-icons" href="https://github.com/luarrekcah">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a className="social-icons" href="https://wa.me/+556892402096">
+          <FontAwesomeIcon icon={faWhatsapp} />
+        </a>
+        <a className="social-icons" href="https://www.instagram.com/luarrekcah/">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a className="social-icons" href="https://discordapp.com/users/701953428510736396">
+          <FontAwesomeIcon icon={faDiscord} />
+        </a>
     </footer>
   );
 };
