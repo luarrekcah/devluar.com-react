b4ca3b3e850e86de8126343e465eac70b641a582
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-04-09 19:51:51 -0500

    [added] search input

--- src/pages/Store/components/AllProducts.jsx
+++ src/pages/Store/components/AllProducts.jsx
@@ -1,7 +1,7 @@
 import { Grid, Paper } from "@mui/material";
 import { Container } from "@mui/system";
 import React, { useEffect, useState } from "react";
-import { Spinner } from "react-bootstrap";
+import { Form, Spinner } from "react-bootstrap";
 import { styled } from "@mui/material/styles";
 import axios from "axios";
 import ProductItem from "./compsChild/ProductItem";
@@ -9,6 +9,15 @@ import ProductItem from "./compsChild/ProductItem";
 const AllProducts = ({ onOpenModal }) => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchChange = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const filteredProducts = products.filter((product) =>
+    product.data.nome.toLowerCase().includes(searchValue.toLowerCase())
+  );
 
   useEffect(() => {
     axios
@@ -42,6 +51,17 @@ const AllProducts = ({ onOpenModal }) => {
 
   return (
     <Container style={{ marginTop: 40 }}>
+      <div
+        style={{ display: "flex", justifyContent: "center", paddingBottom: 20 }}
+      >
+        <Form.Control
+          type="text"
+          placeholder="Pesquisar produto"
+          value={searchValue}
+          onChange={handleSearchChange}
+          style={{ width: "50%" }}
+        />
+      </div>
       <Grid
         container
         spacing={1}
@@ -49,15 +69,15 @@ const AllProducts = ({ onOpenModal }) => {
         justifyContent="center"
         alignItems="center"
       >
-        {products.map((item, index) => {
+        {filteredProducts.map((item, index) => {
           return (
-            <div style={{margin: 5}}>
-            <ProductItem
-              key={index}
-              item={item}
-              index={index}
-              onOpenModal={onOpenModal}
-            />
+            <div style={{ margin: 5 }}>
+              <ProductItem
+                key={index}
+                item={item}
+                index={index}
+                onOpenModal={onOpenModal}
+              />
             </div>
           );
         })}
