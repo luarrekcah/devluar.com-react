7f343e18f937873cd9c500df2b725563a5ef8979
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-06-16 16:59:12 -0500

    teste

--- next.config.js
+++ next.config.js
@@ -8,6 +8,19 @@ const nextConfig = {
           },
         ],
       },
+      async headers() {
+        return [
+          {
+            source: '/(.*)',
+            headers: [
+              {
+                key: 'Access-Control-Allow-Origin',
+                value: '*',
+              },
+            ],
+          },
+        ];
+      },
 }
 
 module.exports = nextConfig
